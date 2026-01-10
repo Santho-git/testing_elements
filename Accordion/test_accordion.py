a616f2b94993275bd1d792a6951ebb507406ff35
SanthoshiValli G S <santhoshivalli854@gmail.com>
2026-01-10 15:52:12 +0530

    Update test_accordion.py

--- Accordion/test_accordion.py
+++ Accordion/test_accordion.py
@@ -1,150 +1,152 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-import time
+import pytest
 
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get("https://mui.com/material-ui/react-accordion/")
-wait = WebDriverWait(driver, 5)
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://mui.com/material-ui/react-accordion/")
+    yield driver
+    driver.quit()
 
-# for navigating to the section expanded by default accordion for visualizing
-navi = driver.find_element(By.XPATH, "(//span[contains(text(),'Expanded by default')])[2]")
-navi.click()
+@pytest.fixture
+def wait(driver):
+    return WebDriverWait(driver, 5)
 
+@pytest.fixture
+def accordion(driver):
+    navi = driver.find_element(
+        By.XPATH, "(//span[contains(text(),'Expanded by default')])[2]"
+    )
+    navi.click()
+
+    accordion = driver.find_element(
+        By.XPATH, "(//div[starts-with(@id, 'demo-_R_')])[3]"
+    )
+    return accordion
 #---------------------------Verify the expanded by default accordion is present--------------------------
-# finding the element where the element is present and checking if the element is getting displayed or not
-accordion = driver.find_element(By.XPATH, "(//div[starts-with(@id, 'demo-_R_')])[3]")
-assert accordion.is_displayed()
-print("element found")
+def test_expanded_by_default_accordion_is_present(accordion):
+    assert accordion.is_displayed()
 
 #--------------------------------Verify there are two sections inside the accordion---------------------------------
-section = accordion.find_elements(By.XPATH, ".//button[starts-with(@id,'panel')]")
-total = len(section)
-assert total == 2
-print("the number of sections present is", total)
+def test_accordion_has_two_sections(accordion):
+    sections = accordion.find_elements(
+        By.XPATH, ".//button[starts-with(@id,'panel')]"
+    )
+    assert len(sections) == 2
 
 #--------------------------Verify there are headers present in each section-------------------------------------
-text_find = accordion.find_elements(By.XPATH, ".//span[contains(@class,'MuiTypography-root')]")
-all_texts = [span.text.strip() for span in text_find]
-print(all_texts)
-EXPECTED_LIST = ["Expanded by default", "Header"]
-assert all_texts == EXPECTED_LIST, f"Expected {EXPECTED_LIST}, got {all_texts}"
-time.sleep(1)
+
+def test_verify_headers(accordion):
+    text_find = accordion.find_elements(By.XPATH, ".//span[contains(@class,'MuiTypography-root')]")
+    all_texts = [span.text.strip() for span in text_find]
+    expected_texts= ["Expanded by default", "Header"]
+    assert all_texts == expected_texts, f"Expected {expected_texts}, got {all_texts}"
 
 #----------------------------A Helper Function--------------------------------
-def assert_section_state(section_button, should_be_expanded):
+def assert_section_state(wait, section_button, should_be_expanded):
+    xpath = ".//*[contains(@class,'Mui-expanded')]"
+
     if should_be_expanded:
         wait.until(
-            lambda d: len(
-                section_button.find_elements(
-                    By.XPATH, ".//*[contains(@class,'Mui-expanded')]"
-                )
-            ) > 0
+            lambda d: len(section_button.find_elements(By.XPATH, xpath)) > 0
         )
     else:
         wait.until(
-            lambda d: len(
-                section_button.find_elements(
-                    By.XPATH, ".//*[contains(@class,'Mui-expanded')]"
-                )
-            ) == 0
+            lambda d: len(section_button.find_elements(By.XPATH, xpath)) == 0
         )
+
 #-------------------------------------Verify by default, the section 1 is expanded---------------------------------
-expanded_element = accordion.find_element(By.XPATH,"(.//button[starts-with(@id,'panel')])[1]")
-assert_section_state(expanded_element, should_be_expanded = True)
-print("First section is expanded as expected")
+def test_first_section_is_expanded_by_default(accordion, wait):
+    section_button = accordion.find_element(By.XPATH, "(.//button[starts-with(@id,'panel')])[1]")
+    assert_section_state(wait, section_button, should_be_expanded=True)
 
 #----------------------------------Verify by default, the section 2 is not expanded-------------------------------
-collapsed_element = accordion.find_element(By.XPATH, "(.//button[starts-with(@id,'panel')])[2]")
-assert_section_state(collapsed_element, should_be_expanded = False)
-print("Second section is collapsed as expected")
+def test_section_2_collapsed_by_default(accordion, wait):
+    collapsed_element = accordion.find_element(By.XPATH, "(.//button[starts-with(@id,'panel')])[2]")
+    assert_section_state(wait, collapsed_element, should_be_expanded=False)
 
 #-----------------------Verify clicking on the accordion header expands/closes the section----------------------------
-# Verification of header behavior for section 1
-sect1_click = accordion.find_element(By.XPATH, "(.//button[starts-with(@id,'panel')])[1]")
+def test_clicking_header_expands_and_collapses_sections(accordion, wait):
 
-# initially section 1 is expanded
-sect1_click.click()
+    # -------- Section 1 --------
+    sect1 = accordion.find_element(By.XPATH, "(.//button[starts-with(@id,'panel')])[1]")
 
-# after clicking, section 1 will be collapsed
-assert_section_state(sect1_click, should_be_expanded = False)
-print("First section closed successfully")
+    # initially expanded → click → collapse
+    sect1.click()
+    assert_section_state(wait, sect1, should_be_expanded=False)
 
-# expanding the section1 again to verify if it is expanding
-sect1_click.click()
-assert_section_state(sect1_click, should_be_expanded = True)
+    # click again → expand
+    sect1.click()
+    assert_section_state(wait, sect1, should_be_expanded=True)
 
-# Verification of header behavior for section 2
-sect2_click = accordion.find_element(By.XPATH, "(.//button[starts-with(@id,'panel')])[2]")
+    # -------- Section 2 --------
+    sect2 = accordion.find_element(By.XPATH, "(.//button[starts-with(@id,'panel')])[2]")
 
-# initially section 1 is expanded
-sect2_click.click()
+    # initially collapsed → click → expand
+    sect2.click()
+    assert_section_state(wait, sect2, should_be_expanded=True)
 
-# after clicking, section 2 will be opened
-assert_section_state(sect2_click, should_be_expanded = True)
-print("Second section opened successfully")
+    # click again → collapse
+    sect2.click()
+    assert_section_state(wait, sect2, should_be_expanded=False)
 
-# collapsing section 2 after expanding
-sect2_click.click()
-assert_section_state(sect2_click, should_be_expanded = False)
-print("Second section closed successfully")
-
-sect1_click.click()
-
-#--------------------------------------Verify multi open expansion-------------------------------------------
-section = accordion.find_elements(By.XPATH, ".//button[starts-with(@id,'panel')]")
-expanded_sections = []
-for ele in section:
-    ele.click()
-    expanded_sections.append(ele)
-    for expanded in expanded_sections:
-        assert_section_state(expanded, should_be_expanded=True)
-# here, both sections are in open
+def test_multiple_sections_can_be_expanded(accordion, wait):
+    section = accordion.find_elements(By.XPATH, ".//button[starts-with(@id,'panel')]")
+    section2 = accordion.find_element(By.XPATH, "(.//button[starts-with(@id,'panel')])[2]")
+    section2.click()
+    expanded_sections = []
+    for ele in section:
+        expanded_sections.append(ele)
+        for expanded in expanded_sections:
+            assert_section_state(wait, expanded, should_be_expanded=True)
 
 #-------------Verify clicking on the icon present at the side, expands and collapses the section----------------------
-icon_id = accordion.find_elements(By.XPATH, ".//span[contains(@class,'MuiAccordionSummary-expandIconWrapper')]")
-for icon in icon_id:
-    # find the parent section button of this icon
-    section_button = icon.find_element(By.XPATH, "./ancestor::button")
 
-    # click → should close
-    icon.click()
-    assert_section_state(section_button, should_be_expanded=False)
-    print("Section collapsed via icon")
+def test_icon_click_expands_and_collapses_section(accordion, wait):
+    section2_click = accordion.find_element(By.XPATH, "(.//button[starts-with(@id,'panel')])[2]")
+    section2_click.click()
+    icon_id = accordion.find_elements(By.XPATH, ".//span[contains(@class,'MuiAccordionSummary-expandIconWrapper')]")
 
-    assert "Mui-expanded" not in icon.get_attribute("class")
+    for icon in icon_id:
+        # find the parent section button of this icon
+        section_button = icon.find_element(By.XPATH, "./ancestor::button")
 
-    transform_value1 = icon.value_of_css_property("transform")
+        # click → should close
+        icon.click()
+        assert_section_state(wait, section_button, should_be_expanded=False)
 
-    # click again → should open
-    icon.click()
-    assert_section_state(section_button, should_be_expanded=True)
-    print("Section expanded via icon")
+        assert "Mui-expanded" not in icon.get_attribute("class")
 
-    assert "Mui-expanded" in icon.get_attribute("class"), "Icon is inverted unexpectedly"
+        transform_before = icon.value_of_css_property("transform")
 
-    transform_value2 = icon.value_of_css_property("transform")
+        # click again → should open
+        icon.click()
+        assert_section_state(wait, section_button, should_be_expanded=True)
 
-    # -----------------Verify that the icon becomes inverted (from 'v' to '^') when clicked on------------------
-    if transform_value1 != transform_value2:
-        assert "success"
-    else:
-        assert "failed"
+        assert "Mui-expanded" in icon.get_attribute("class"), "Icon is inverted unexpectedly"
 
-# at this point, both the sections in accordion is expected to be in open
+        transform_after = icon.value_of_css_property("transform")
+
+        # -----------------Verify that the icon becomes inverted (from 'v' to '^') when clicked on------------------
+        assert transform_before != transform_after
+        # at this point, both the sections in accordion is expected to be in open
 
 #-----------------Verify the content is fully visible when expanded------------------------------
 # start from the button, climb up to its accordion, then search inside it for the content
 
-def assert_content_visible(content_section_button, content_should_be_visible):
-    content= content_section_button.find_element(
+def assert_content_visible(wait, section_button, should_be_visible):
+
+    content = section_button.find_element(
         By.XPATH,
         "./ancestor::h3"
         "/following-sibling::div[contains(@class,'MuiCollapse-root')]"
         "//div[contains(@class,'MuiAccordionDetails-root')]"
     )
-    if content_should_be_visible:
+
+    if should_be_visible:
         wait.until(lambda d: content.is_displayed())
         assert content.is_displayed(), "Accordion content is not visible"
         assert content.size["height"] > 0, "Accordion content is not fully expanded"
@@ -153,32 +155,30 @@ def assert_content_visible(content_section_button, content_should_be_visible):
         assert not content.is_displayed(), "Accordion content is visible"
 
 
-# verification of content for section 1
-# expanded view
-content_sect1_button = accordion.find_element(
-    By.XPATH, "(.//button[starts-with(@id,'panel')])[1]"
-)
+def test_accordion_content_visibility(accordion, wait):
 
-assert_section_state(content_sect1_button, should_be_expanded=True)
-assert_content_visible(content_sect1_button, content_should_be_visible= True)
+    content_section1 = accordion.find_element(
+        By.XPATH, "(.//button[starts-with(@id,'panel')])[1]"
+    )
+    content_section2 = accordion.find_element(
+        By.XPATH, "(.//button[starts-with(@id,'panel')])[2]"
+    )
 
+    # Section 1 expanded by default
+    assert_section_state(wait, content_section1, should_be_expanded=True)
+    assert_content_visible(wait, content_section1, should_be_visible=True)
 
-# verification of content for section 2
-content_sect2_button = accordion.find_element(
-    By.XPATH, "(.//button[starts-with(@id,'panel')])[2]"
-)
+    # Expand section 2
+    content_section2.click()
+    assert_section_state(wait, content_section2, should_be_expanded=True)
+    assert_content_visible(wait, content_section2, should_be_visible=True)
 
-# expanded view
-assert_section_state(content_sect2_button, should_be_expanded=True)
-assert_content_visible(content_sect2_button, content_should_be_visible= True)
+    # Collapse section 1
+    content_section1.click()
+    assert_section_state(wait, content_section1, should_be_expanded=False)
+    assert_content_visible(wait, content_section1, should_be_visible=False)
 
-content_sect1_button.click()
-
-assert_section_state(content_sect1_button, should_be_expanded=False)
-assert_content_visible(content_sect1_button, content_should_be_visible= False)
-
-content_sect2_button.click()
-
-assert_section_state(content_sect2_button, should_be_expanded=False)
-assert_content_visible(content_sect2_button, content_should_be_visible= False)
-driver.quit()
+    # Collapse section 2
+    content_section2.click()
+    assert_section_state(wait, content_section2, should_be_expanded=False)
+    assert_content_visible(wait, content_section2, should_be_visible=False)
